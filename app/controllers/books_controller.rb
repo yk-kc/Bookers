a6ec393fcd50b0f0fdda9e41b9568f64f6d8ea9f
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-03-09 08:34:56 +0000

    [Finish]create機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,20 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to '/books/:id'
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
